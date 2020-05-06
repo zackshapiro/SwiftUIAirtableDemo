52663cc63ecb01cab727a3c64ae1cbafd35f475f
Zack Shapiro <zack@zackshapiro.com>
2020-05-06 10:33:32 -0400

    Initial Commit

---
+++ SwiftUIAirtableDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIAirtableDemo
+//
+//  Created by Zack Shapiro on 5/6/20.
+//  Copyright © 2020 Zack Shapiro. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
